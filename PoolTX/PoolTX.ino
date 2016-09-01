3c02fb5469ada9f8c343719d7ce16503e6473201
wusiv <yusufkal@gmail.com>
2016-09-01 21:02:44 +0300

    Update PoolTX.ino

--- PoolTX/PoolTX.ino
+++ PoolTX/PoolTX.ino
@@ -1,28 +1,4 @@
-/*
-* Name:		PoolTX.ino
-* Created:	24/07/2016 14:46
-* Author:	Yusuf KALSEN
-* URL: http://github.com/wusiv
 
-* Arduino Pool Water Level is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published
-* by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Arduino Pool Water Level is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with Arduino Pool Water Level. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
-
-
-/* pull down resistor
-*  Pull-Down Resistor. 
-*/
 /*
 * Name:		PoolTX.ino
 * Created:	24/07/2016 14:46
